193e852d7a840790ae2578a15781a6eda036ed64
ruhamabek <ruhamauzubo@gmail.com>
2025-08-05 17:59:55 +0300

    workflow starter

---
+++ actions/workflows/createWorkflow.ts
@@ -0,0 +1,45 @@
+"use server";
+
+import { auth } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+import { createWorkflowSchema, createWorkflowtype } from "@/schema/workflow";
+import { WorkflowStatus } from "@/types/workflow";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+
+export async function CreateWorkflow(
+    form: createWorkflowtype
+){
+    const {success , data} = createWorkflowSchema.safeParse(form);
+           
+     if(!success){
+        throw new Error("invalid form data");
+     }
+     
+    const session = await auth.api.getSession({
+        headers: await headers() // you need to pass the headers object.
+    })    
+     
+     const userId = session?.user.id;
+
+
+    if(!userId){
+        throw new Error("unauthenticated");
+    }
+
+    const result = await prisma.workflow.create({
+        data:{
+            userId,
+            status: WorkflowStatus.DRAFT,
+            definition: "TODO",
+            ...data,
+        },
+    });
+
+    if(!result){
+        throw new Error("Failed to create workflow");
+    }
+     
+   redirect(`/workflow/editor/${result.id}`)
+}; 
--- app/(dashboard)/dashboard/workflows/_components/CreateWorkflowDialog.tsx
+++ app/(dashboard)/dashboard/workflows/_components/CreateWorkflowDialog.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react"
+import React, { useCallback, useState } from "react"
 import {
   Dialog,
   DialogContent,
@@ -8,10 +8,52 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button";
+import { IconFidgetSpinner, IconLayersIntersect } from "@tabler/icons-react";
+import CustomDialogHeader from "@/components/CustomDialogHeader";
+import { useForm } from "react-hook-form";
+import { createWorkflowSchema, createWorkflowtype } from "@/schema/workflow";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Textarea } from "@/components/ui/textarea";
+import { useMutation } from "@tanstack/react-query";
+import { CreateWorkflow } from "@/actions/workflows/createWorkflow";
+import { toast } from "sonner";
+import { Loader2 } from "lucide-react";
 
 function CreateWorkflowDialog({triggerText} : {triggerText?: string}){
     const [open , setOpen] = useState(false);
+
+    const form = useForm<createWorkflowtype>({
+        resolver: zodResolver(createWorkflowSchema),
+        defaultValues: {},
+    });
+    
+    const {mutate , isPending} = useMutation(
+        {
+            mutationFn: CreateWorkflow,
+            onSuccess: () => {
+                toast.success("Workflow Created." , {id: "create-workflow"})
+            },
+            onError: () => {
+                toast.error("Failed to create workflow" , {id: "create-workflow"})
+            },
+        }
+    )
+    const onSubmit = useCallback((values:createWorkflowtype) => {
+        toast.loading("Creating workflow..." ,  {id: "create-workflow"});
+        mutate(values)
+    }, [mutate]);
 
     return (
             <Dialog open={open} onOpenChange={setOpen}>
@@ -22,6 +64,58 @@ function CreateWorkflowDialog({triggerText} : {triggerText?: string}){
                         }
                     </Button> 
                 </DialogTrigger>
+                <DialogContent className="px-0">
+                    <CustomDialogHeader
+                         icon={IconLayersIntersect}
+                         title="Create workflow"
+                         subTitle="Start building your workflow"
+                     />
+                     <div className="p-6">
+                       <Form {...form}>
+                            <form className="space-y-8" onSubmit={form.handleSubmit(onSubmit)}>
+                                <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                    <FormLabel>Name
+                                        <p className="text-xs text-muted-foreground">(required)</p>
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="eg. Flo" {...field} />
+                                    </FormControl>
+                                    <FormDescription>
+                                        Choose a Desciptive name
+                                    </FormDescription>
+                                    <FormMessage />
+                                    </FormItem>
+                                )}
+                                />
+                                <FormField
+                                control={form.control}
+                                name="description"
+                                render={({ field }) => (
+                                    <FormItem>
+                                    <FormLabel>Description
+                                       <p className="text-xs text-muted-foreground">(optional)</p>
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Textarea className="resize-none" {...field} placeholder="eg. scrape shoe prices..." {...field} />
+                                    </FormControl>
+                                    <FormDescription>
+                                       Provide a brief description of what your workflow does
+                                    </FormDescription>
+                                    <FormMessage />
+                                    </FormItem>
+                                )}
+                                />
+                                <Button type="submit" disabled={isPending}> {!isPending  && "Submit"} 
+                                                                            {isPending && <Loader2 className="animate-spin"/>}
+                                </Button>
+                            </form>
+                        </Form>
+                     </div>
+                </DialogContent>
             </Dialog>
     )
 }
